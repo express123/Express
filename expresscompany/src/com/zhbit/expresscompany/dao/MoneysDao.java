a13aade18f9da4be654e327860576d662aa7bd3d
812419396 <812419396@qq.com>
2016-06-07 19:47:10 +0800

    炯

--- expresscompany/src/com/zhbit/expresscompany/dao/MoneysDao.java
+++
@@ -1,7 +0,0 @@
-package com.zhbit.expresscompany.dao;
-
-import com.zhbit.expresscompany.domain.Moneys;
-
-public interface MoneysDao {
-	Moneys getMoneyByMid(int mid);
-}
--- expresscompany/src/com/zhbit/expresscompany/service/MoneysSerive.java
+++
@@ -1,7 +0,0 @@
-package com.zhbit.expresscompany.service;
-
-import com.zhbit.expresscompany.domain.Moneys;
-
-public interface MoneysSerive {
-	 Moneys getMoneyByMid(int mid);
-}
